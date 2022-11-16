3ca435f841a24d3a7f3c7279dab2a36c9b65fb2c
Brendan Op't Root <optroot@gmail.com>
2022-11-16 00:15:12 -0500

    [DSIP-3995] Config parser
    Squashed commit of the following:
    
    commit 3132354fda9f1925efd77db5f9d5071b9fca1483
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Wed Nov 9 11:38:13 2022 -0500
    
        update
    
    commit 8140fd8c01bfeba0a2b394097251062ac340e4df
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Mon Nov 7 12:42:18 2022 -0500
    
        Added unittests
    
    commit 05bbd97fe947a50e29dd38cbe637f593ded32c2a
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Wed Nov 2 13:56:32 2022 -0400
    
        update documentation
    
    commit f2f6e8ba873571876efa46b4aeaa040cb16a7cae
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Wed Nov 2 13:33:43 2022 -0400
    
        code to merge two dictionaries
    
    commit 04c6fe470ee668f5a45341328f73617dac9aa25d
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Tue Nov 1 15:52:21 2022 -0400
    
        updates
    
    commit 3791286d8834bd441e7d94ce2636c9b95491ddc0
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Tue Nov 1 15:51:50 2022 -0400
    
        Updated code.
    
    commit dce9002e0c6c3c1374ec9ead298bd79c2484bf16
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Wed Oct 19 11:51:01 2022 -0400
    
        Adding logging
    
        Included logging for Errors and information.
    
    commit 58326dbfab271d26ce4d3aee5e2ad219abe71e5e
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Tue Oct 18 15:52:42 2022 -0400
    
        additional changes.
    
    commit 6b17d7853cb844f758c29a31827ff5edd8827cff
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Tue Oct 18 15:49:43 2022 -0400
    
        Made changes, added try exception. Logging errors in a new file.
    
    commit f88bc5630d5d77de01f50de84d71e32bb182db21
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Mon Oct 17 20:41:00 2022 -0400
    
        documentation added.
    
    commit 2932e00e90db163ce2d7c94ecc87a1e2620b9bd9
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Mon Oct 17 15:40:42 2022 -0400
    
        updated the class.
    
    commit 1db6658d03a7c40ef0a61f6633e127f1ea3ba321
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Fri Oct 14 09:24:48 2022 -0400
    
        removed spaces
    
    commit 5a43ceb13646dbd3c770d61641dfcf3c9e1b96a3
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Thu Oct 13 14:18:49 2022 -0400
    
        code ready for review.
    
        added try/catch and documented code.
    
    commit dd8e240df2cb14314031cdd39c83c6a2232c5595
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Thu Oct 13 13:14:51 2022 -0400
    
        fixed bugs
    
    commit 66bd8937d6f6a2ec45558ef08675d90c2dc20ee9
    Author: MurtazaBadshahAdastra <115025495+MurtazaBadshahAdastra@users.noreply.github.com>
    Date:   Thu Oct 13 10:19:35 2022 -0400
    
        developing the config parser.
    
        Created dummy file and also working on the bug fixing the classes.

--- pynutrien/env/config.py
+++ pynutrien/env/config.py
@@ -1,1 +1,174 @@
+import logging
+import json
+import tomli        
+import configparser 
+import yaml
+from pathlib import Path
 
+filepath = Path(r"C:\Users\Murtaza.Badshah\Documents\GitHub\insights-framework\pynutrien\env")
+
+logging.basicConfig(filename=filepath/'logfile.txt', level=logging.INFO,
+                format='%(asctime)s:%(levelname)s:%(message)s')
+
+class ConfigReader:
+    """
+    A class used for reading a config file.
+    ...
+
+    Attributes
+    ----------
+    filename: str
+        this is the name of the file along with the extension
+
+    Methods
+    -------
+    parseConfig(self)
+        reads the file from the specified folder.
+        based on the file extension converts the
+        file into a python dictionary
+    readFileExt(self)
+        takes the entered file name splits the
+        text and returns just the extension name.
+    """
+
+    def __init__(self, file_string:str) -> None:
+        """
+        init function used to initialize the object of the class
+        ...
+        Args:
+            filename (string): name of the file the user inputs.
+        """
+        self.file_string = file_string
+        logging.info('Created file string: {}'.format(self.file_string))
+
+
+    def parse_json(self) -> dict:
+        """This method parses the string value (json string)
+        into a python dictionary
+
+        Returns:
+            dict: a python dictionary of the json string.
+        """
+        try:
+            output = json.loads(self.file_string)
+            logging.info('Success! The json string was converted into {}'.format(type(output)))
+            return output
+        except Exception as e:
+            logging.error("Incorrect Format of string variable", exc_info=True)
+            
+
+
+    def parse_ini(self) -> dict:
+        """This method takes the .ini files as a string
+        value and returns a python dictionary by using
+        the config parser package.
+
+        Returns:
+            dict: a python dictionary of the ini string
+            (IMPORTANT: The specific key values need
+            to be specified when you call the method
+            in square brackets for example 
+            VARIABLENAME['Key']['subkey])
+        """
+        try:
+            config = configparser.ConfigParser()
+            config.read_string(self.file_string)
+            # Code referenced from https://stackoverflow.com/questions/1773793/convert-configparser-items-to-dictionary
+            output = {s:dict(config.items(s)) for s in config.sections()} 
+            logging.info('Success! The ini string was converted into {}'.format(type(output)))
+            return output
+        except Exception as e:
+            logging.error("Incorrect Format of string variable", exc_info=True)
+            
+            
+
+    def parse_yaml(self) -> dict:
+        """Uses the yaml package to interpret the string
+        and convert it into a python dictionary.
+
+        Returns:
+            dict: a python dictionary of the yaml string.
+        """
+        try:
+            output = yaml.load(self.file_string, Loader=yaml.Loader)
+            logging.info('Success! The yaml string was converted into {}'.format(type(output)))
+            return output
+        except Exception as e:
+            logging.error("Incorrect Format of string variable", exc_info=True)
+
+
+    def parse_toml(self) -> dict:
+        """Uses the tomli library to read the toml string
+        and convert it into a python dictionary.
+
+        Returns:
+            dict: a python dictionary of the toml string.
+        """
+        try:
+            output = tomli.loads(self.file_string)
+            logging.info('Success! The toml string was converted into {}'.format(type(output)))
+            return output
+        except Exception as e:
+            logging.error("Incorrect Format of string variable", exc_info=True)
+
+
+if __name__ == "__main__":
+    json_text = """
+    {
+    "fruit": { 
+        "apple": {
+            "size": "small",
+            "color": "red",
+            "country": "USA"
+        },
+        "banana":{
+            "size": "medium",
+            "color": "yellow",
+            "country": "Fiji"
+        },
+        "orange":{
+            "size": "large",
+            "color": "orange",
+            "country": "Egypt"
+        }
+    }
+}
+    """
+    yaml_text = """
+        name: "Vishvajit"
+        age: 23
+        address: Noida
+        Occupation: Software Developer
+        Skills:
+        - Python
+        - Django
+        - Flask
+        - FastAPI
+        - DRF ( Django Rest Framework )
+    """
+    toml_text = """
+        [user]
+        player_x.color = "blue"
+        player_o.color = "green"
+
+        [constant]
+        board_size = 3
+
+        [server]
+        url = "https://tictactoe.example.com"
+    """
+    ini_text = """
+        [apple]
+        size = small
+        color = red
+        country = USA
+    """
+    sample_dict = {"Vegetable": {"carrot":{"size": "cylindrical", "color": "orange", "country": "America"}}}
+    a = ConfigReader(json_text)
+    b = ConfigReader(ini_text)
+    c = ConfigReader(yaml_text)
+    d = ConfigReader(toml_text)
+    print(a.merge_dictionaries(sample_dict))
+    #print(c.parse_yaml())
+    #print(d.parse_toml())
+    #print(b.parse_ini())
