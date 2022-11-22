24526aa1e32d2f53e27d99ab1ca6aab5f54083e6
Brendan Op't Root <brendan.optroot@nutrien.com>
2022-11-22 10:38:46 -0500

    [DSIP-3994] Added optional argument parser

---
+++ pynutrien/env/args.py
@@ -0,0 +1,96 @@
+import argparse
+import logging
+import sys
+from typing import Any
+
+class ArgParser(argparse.ArgumentParser):
+    def __init__(self,*args, **kwargs) -> None:
+        """ This is an inherited class from argparse library with additional methods to add and parse custom arguments.
+        Also, a custom logger has also been set up when initializing this class
+        """
+        super().__init__(*args, **kwargs)
+        self.logger = kwargs["logger"] if "logger" in kwargs else logging.getLogger(__name__)
+        self.handler = kwargs["handler"] if "handler" in kwargs else logging.StreamHandler(
+            sys.stdout)
+        self.logging_level = kwargs["logging_level"] if "logging_level" in kwargs else 20
+        self.format_string = '%(asctime)s::%(module)s::%(funcName)s::%(lineno)d::%(message)s'
+        self.logger_format = logging.Formatter(
+            kwargs["logger_format"] if "logger_format" in kwargs else self.format_string)
+
+        self.handler.setFormatter(self.logger_format)
+        self.logger.setLevel(self.logging_level)
+        self.logger.addHandler(self.handler)
+
+    def add_custom_arguments(self, args: dict) -> None:
+        """This method takes a dictionary of custom arguments and add to the system to define how those arguments are taken on the command line to be parsed
+        Args:
+            args (dict): a list of arguments in dictionary format with key is the argument name and value is more details for the command-line argument
+            in terms of action/default value/ required (if applicable)
+
+        Raises:
+            ValueError: when custom input arguments are not sufficient with information (not in dictionary format)
+        """
+        if not isinstance(args, dict):
+            error_msg = 'Arguments passed in needs to be in dictionary format with key-value pair(s)'
+            self.logger.exception(error_msg)
+            raise ValueError(error_msg)
+
+        for arg_name, arg_value in args.items():
+            self.add_custom_argument(arg_name, arg_value)
+
+    def add_custom_argument(self, arg_name: str, arg_value: Any) -> None:
+        """This method is a sub-step in the loop of adding custom arguments. This takes both argument name and its value detail to define the command-line argument
+
+        Args:
+            arg_name (str): name of the custom argument to be added
+            arg_value (Any): details (if any) of the custom argument. It could be a boolean (to store argument as true/false), a default value, a dictionary with key acts as a
+            parameter of the add_argument() method (e.g. {required: True}), or it could be None (adding the argument without any specifications)
+        """
+        if arg_value:
+            if isinstance(arg_value, bool):
+                self.add_argument(arg_name, action=f'store_{str(arg_value).lower()}')
+            elif isinstance(arg_value, dict):
+                self.add_argument(arg_name, **arg_value)
+            else:
+                self.add_argument(arg_name, default=arg_value, type=type(arg_value))
+        else:
+            self.add_argument(arg_name)
+
+    def parse(self, args: list[str] = None) -> dict:
+        """This method convert custom argument strings to object and assign them as attributes of a result dictionary
+
+        Args:
+            args (list[str], optional): list of argument/option strings that wish to be parsed. Defaults to None (value is taken from sys.argv).
+
+        Raises:
+            ValueError: when there's no arguments passed before parsing
+
+        Returns:
+            dict: dictionary of results with argument name being the key and its parsed value being the value
+        """
+        parsed_dict = None
+        existing_args = sys.argv[1:]
+        if existing_args is None or len(existing_args) == 0:
+            container = self._actions
+            if container is None or len(container) < 2:
+                error_msg = "There's no arguments passed in for parsing"
+                self.logger.exception(error_msg)
+                raise ValueError(error_msg)
+        parsed, unknown = self.parse_known_args(args=args)
+        parsed_dict = vars(parsed)
+        return parsed_dict
+
+    def parse_glue_args(self, glue_args: list[str] = []) -> dict:
+        """This method is used specifically for parsing glue arguments
+
+        Args:
+            glue_args (list[str], optional): list of glue arguments to be parsed. Defaults to [].
+
+        Returns:
+            dict: dictionary of both parsed glue arguments and custom arguments
+        """
+        from awsglue.utils import getResolvedOptions
+
+        glue_args = getResolvedOptions(sys.argv, glue_args)
+        args = self.parse(sys.argv)
+        return {**glue_args, **args}
---
+++ pynutrien/env/test_args.py
@@ -0,0 +1,89 @@
+import sys
+import pytest
+from pynutrien.env.args import ArgParser
+
+@pytest.fixture
+def arg_parser():
+    return ArgParser()
+
+# arguments is fully parsed then return a dictionary of parsed argument
+def test_when_args_are_parsed_then_return_dict(arg_parser):
+    args = {
+        '--debug_run': True,
+        '--input_connector': None,
+        '--debug_level': 'INFO',
+        '--value': {'required': True}
+    }
+    arg_parser.add_custom_arguments(args)
+    parsed_dict = arg_parser.parse([
+        '--debug_run',
+        '--input_connector','a',
+        '--debug_level','b',
+        '--value','c'
+        ])
+    assert isinstance(parsed_dict, dict) == True
+    assert parsed_dict['input_connector'] == 'a'
+
+def test_when_args_passed_in_is_not_dict_should_raise_value_error(arg_parser):
+    args = 2
+    with pytest.raises(ValueError):
+        arg_parser.add_custom_arguments(args)
+
+def test_when_args_should_be_parsed_as_bool_only(arg_parser):
+    args = {
+        '--debug_run': True
+    }
+    arg_parser.add_custom_arguments(args)
+    parsed_dict = arg_parser.parse(['--debug_run', 'a'])
+    assert parsed_dict['debug_run'] == True
+    assert parsed_dict['debug_run'] != 'a'
+
+def test_when_args_are_parsed_as_none(arg_parser):
+    args = {
+        '--debug_run': None
+    }
+    arg_parser.add_custom_arguments(args)
+    parsed_dict = arg_parser.parse('--debug_run')
+    assert parsed_dict['debug_run'] == None
+
+def test_when_args_are_set_to_a_default_value(arg_parser):
+    args = {
+        '--debug_run': 'INFO'
+    }
+    arg_parser.add_custom_arguments(args)
+    parsed_dict = arg_parser.parse('--debug_run')
+    assert parsed_dict['debug_run'] == 'INFO'
+
+def test_when_args_are_set_as_required_but_not_included_should_fail(arg_parser):
+    args = {
+        '--debug_run': {'required': True}
+    }
+    arg_parser.add_custom_arguments(args)
+    with pytest.raises(SystemExit):
+        arg_parser.parse()
+
+    with pytest.raises(SystemExit):
+        arg_parser.parse(['--value'])
+
+# not exist in arguments but exist in option (partial parse - only parse the existing arguments)
+def test_when_args_are_partially_parsed_should_work(arg_parser):
+    args = {
+        '--debug_run': 'INFO'
+    }
+    arg_parser.add_custom_arguments(args)
+    parsed_dict = arg_parser.parse(['--debug_run','DEBUG','--value'])
+    assert parsed_dict['debug_run'] == 'DEBUG'
+    assert 'value' not in parsed_dict
+
+# no arguments are added/existed before parsing should raise a ValueError
+def test_when_arguments_is_empty_before_parsing(arg_parser):
+    args = {}
+    existing_args = sys.argv[1:]
+
+    arg_parser.add_custom_arguments(args)
+    if existing_args is None or len(existing_args) == 0:
+        with pytest.raises(ValueError):
+            arg_parser.parse()
+
+        with pytest.raises(ValueError):
+            arg_parser.parse(['--debug-run','--config2'])
