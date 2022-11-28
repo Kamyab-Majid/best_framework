f8038f7f55f5b4c8a6f8ac680345fd154f32587b
vynguyenu <113533744+vynguyenu@users.noreply.github.com>
2022-11-28 11:24:56 -0500

    [DSIP-4170] config parser testing and debugging (#23)
    * clean up args code
    
    * adjusted docstring and some minor things
    
    * change code functionality so that it has one main parse method
    
    * add tests and exception handling
    
    * move tests_config.py to tests
    
    * added exception handling from Kates code and minor adjustments
    
    * reformat
    
    * finalize and some minor fix to get test work
    
    Co-authored-by: Katerina Miazga <katerina.miazga@nutrien.com>
    Co-authored-by: boptroot <113541637+boptroot@users.noreply.github.com>

--- pynutrien/env/args.py
+++ pynutrien/env/args.py
@@ -4,8 +4,13 @@ import sys
 from typing import Any
 
 class ArgParser(argparse.ArgumentParser):
+    """This class inherited from the argparse Python library acts as an argument parser
+    giving user an interface to fully or partially parse arguments
+    (a similar example of this class in AWS Glue is getResolvedOptions)
+    """
     def __init__(self,*args, **kwargs) -> None:
-        """ This is an inherited class from argparse library with additional methods to add and parse custom arguments.
+        """ This is an inherited class from argparse library with additional methods to
+        add and parse custom arguments.
         Also, a custom logger has also been set up when initializing this class
         """
         super().__init__(*args, **kwargs)
@@ -22,16 +27,20 @@ class ArgParser(argparse.ArgumentParser):
         self.logger.addHandler(self.handler)
 
     def add_custom_arguments(self, args: dict) -> None:
-        """This method takes a dictionary of custom arguments and add to the system to define how those arguments are taken on the command line to be parsed
+        """This method takes a dictionary of custom arguments and add to the system to define
+        how those arguments are taken on the command line to be parsed
         Args:
-            args (dict): a list of arguments in dictionary format with key is the argument name and value is more details for the command-line argument
+            args (dict): a list of arguments in dictionary format with key is the argument
+            name and value is more details for the command-line argument
             in terms of action/default value/ required (if applicable)
 
         Raises:
-            ValueError: when custom input arguments are not sufficient with information (not in dictionary format)
+            ValueError: when custom input arguments are not sufficient with information
+            (not in dictionary format)
         """
         if not isinstance(args, dict):
-            error_msg = 'Arguments passed in needs to be in dictionary format with key-value pair(s)'
+            error_msg = \
+                'Arguments passed in needs to be in dictionary format with key-value pair(s)'
             self.logger.exception(error_msg)
             raise ValueError(error_msg)
 
@@ -39,12 +48,15 @@ class ArgParser(argparse.ArgumentParser):
             self.add_custom_argument(arg_name, arg_value)
 
     def add_custom_argument(self, arg_name: str, arg_value: Any) -> None:
-        """This method is a sub-step in the loop of adding custom arguments. This takes both argument name and its value detail to define the command-line argument
+        """This method is a sub-step in the loop of adding custom arguments.
+        This takes both argument name and its value detail to define the command-line argument
 
         Args:
             arg_name (str): name of the custom argument to be added
-            arg_value (Any): details (if any) of the custom argument. It could be a boolean (to store argument as true/false), a default value, a dictionary with key acts as a
-            parameter of the add_argument() method (e.g. {required: True}), or it could be None (adding the argument without any specifications)
+            arg_value (Any): details (if any) of the custom argument.
+            It could be a boolean (to store argument as true/false), a default value, a dictionary with key acts as a
+            parameter of the add_argument() method (e.g. {required: True}),
+            or it could be None (adding the argument without any specifications)
         """
         if arg_value:
             if isinstance(arg_value, bool):
@@ -57,16 +69,19 @@ class ArgParser(argparse.ArgumentParser):
             self.add_argument(arg_name)
 
     def parse(self, args: list[str] = None) -> dict:
-        """This method convert custom argument strings to object and assign them as attributes of a result dictionary
+        """This method convert custom argument strings to object and assign them as attributes of
+        a result dictionary
 
         Args:
-            args (list[str], optional): list of argument/option strings that wish to be parsed. Defaults to None (value is taken from sys.argv).
+            args (list[str], optional): list of argument/option strings that wish to be parsed.
+            Defaults to None (value is taken from sys.argv).
 
         Raises:
             ValueError: when there's no arguments passed before parsing
 
         Returns:
-            dict: dictionary of results with argument name being the key and its parsed value being the value
+            dict: dictionary of results with argument name being the key and
+            its parsed value being the value
         """
         parsed_dict = None
         existing_args = sys.argv[1:]
--- pynutrien/env/config.py
+++ pynutrien/env/config.py
@@ -1,174 +1,108 @@
-import logging
 import json
-import tomli        
-import configparser 
+import configparser
+import tomli
 import yaml
-from pathlib import Path
-
-filepath = Path(r"C:\Users\Murtaza.Badshah\Documents\GitHub\insights-framework\pynutrien\env")
-
-logging.basicConfig(filename=filepath/'logfile.txt', level=logging.INFO,
-                format='%(asctime)s:%(levelname)s:%(message)s')
 
 class ConfigReader:
     """
-    A class used for reading a config file.
-    ...
-
-    Attributes
-    ----------
-    filename: str
-        this is the name of the file along with the extension
-
-    Methods
-    -------
-    parseConfig(self)
-        reads the file from the specified folder.
-        based on the file extension converts the
-        file into a python dictionary
-    readFileExt(self)
-        takes the entered file name splits the
-        text and returns just the extension name.
+    A class used for reading a config file (JSON, YAML, TOML, INI)
     """
 
-    def __init__(self, file_string:str) -> None:
+    def __init__(self) -> None:
         """
         init function used to initialize the object of the class
-        ...
-        Args:
-            filename (string): name of the file the user inputs.
         """
-        self.file_string = file_string
-        logging.info('Created file string: {}'.format(self.file_string))
 
-
-    def parse_json(self) -> dict:
-        """This method parses the string value (json string)
+    def parse(self, file_content: str, file_type: str) -> dict:
+        """
+        This method acts as a main parser to parse a string content of a given file type
         into a python dictionary
 
+        Args:
+            file_content (str): The content string of the json file to be parsed.
+            file_type (str): The type of the config file to be read/parse
+            ('json', 'yaml', 'toml', 'ini')
+
         Returns:
-            dict: a python dictionary of the json string.
+            dict: a python dictionary of the config content.
         """
         try:
-            output = json.loads(self.file_string)
-            logging.info('Success! The json string was converted into {}'.format(type(output)))
-            return output
-        except Exception as e:
-            logging.error("Incorrect Format of string variable", exc_info=True)
-            
+            return eval(f"self.parse_{file_type.strip().lower()}(file_content)")
+        except AttributeError as attr_exc:
+            raise NotImplementedError(
+                f"Reader method has not been implemented for this file type '{file_type}'"
+                ) from attr_exc
 
+    def parse_json(self, file_content: str) -> dict:
+        """
+        This method parses the string content of a JSON file into a python dictionary
 
-    def parse_ini(self) -> dict:
-        """This method takes the .ini files as a string
-        value and returns a python dictionary by using
-        the config parser package.
+        Args:
+            file_content (str): The content string of the json file to be parsed.
 
         Returns:
-            dict: a python dictionary of the ini string
-            (IMPORTANT: The specific key values need
-            to be specified when you call the method
-            in square brackets for example 
-            VARIABLENAME['Key']['subkey])
+            dict: a python dictionary of the json content.
+        """
+        try:
+            return json.loads(file_content)
+        except json.decoder.JSONDecodeError as exc:
+            raise RuntimeError("This file content is not JSON format.") from exc
+
+
+    def parse_ini(self, file_content: str) -> dict:
+        """
+        This method parses the content of the .ini file from a string value
+        to a python dictionary
+
+        Args:
+            file_content (str): The content string of the ini file to be parsed.
+
+        Returns:
+            dict: a python dictionary of the ini string content
+            (IMPORTANT: The specific key values need to be specified when you call the method
+            in square brackets. For example VARIABLENAME['Key']['subkey])
         """
         try:
             config = configparser.ConfigParser()
-            config.read_string(self.file_string)
-            # Code referenced from https://stackoverflow.com/questions/1773793/convert-configparser-items-to-dictionary
-            output = {s:dict(config.items(s)) for s in config.sections()} 
-            logging.info('Success! The ini string was converted into {}'.format(type(output)))
+            config.read_string(file_content)
+            output = {s:dict(config.items(s)) for s in config.sections()}
             return output
-        except Exception as e:
-            logging.error("Incorrect Format of string variable", exc_info=True)
-            
-            
+        except configparser.ParsingError as exc:
+            raise RuntimeError("This file content is not INI format.") from exc
 
-    def parse_yaml(self) -> dict:
-        """Uses the yaml package to interpret the string
+    def parse_yaml(self, file_content: str) -> dict:
+        """
+        This method uses the yaml package to interpret the string content
         and convert it into a python dictionary.
 
+        Args:
+            file_content (str): The content string of the yaml file to be parsed.
+
         Returns:
-            dict: a python dictionary of the yaml string.
+            dict: a python dictionary of the yaml string content.
         """
+        if len(file_content) == 0:
+            return {}
         try:
-            output = yaml.load(self.file_string, Loader=yaml.Loader)
-            logging.info('Success! The yaml string was converted into {}'.format(type(output)))
+            output = yaml.load(file_content, Loader=yaml.Loader)
             return output
-        except Exception as e:
-            logging.error("Incorrect Format of string variable", exc_info=True)
+        except yaml.scanner.ScannerError as exc:
+            raise RuntimeError("This file content is not YAML format.") from exc
 
-
-    def parse_toml(self) -> dict:
-        """Uses the tomli library to read the toml string
+    def parse_toml(self, file_content: str) -> dict:
+        """
+        This method uses the tomli library to read the string content from the toml file
         and convert it into a python dictionary.
 
+        Args:
+            file_content (str): The content string of the toml file to be parsed.
+
         Returns:
-            dict: a python dictionary of the toml string.
+            dict: a python dictionary of the toml string content.
         """
         try:
-            output = tomli.loads(self.file_string)
-            logging.info('Success! The toml string was converted into {}'.format(type(output)))
+            output = tomli.loads(file_content)
             return output
-        except Exception as e:
-            logging.error("Incorrect Format of string variable", exc_info=True)
+        except tomli.TOMLDecodeError as exc:
+            raise RuntimeError("This file content is not TOML format.") from exc
 
-
-if __name__ == "__main__":
-    json_text = """
-    {
-    "fruit": { 
-        "apple": {
-            "size": "small",
-            "color": "red",
-            "country": "USA"
-        },
-        "banana":{
-            "size": "medium",
-            "color": "yellow",
-            "country": "Fiji"
-        },
-        "orange":{
-            "size": "large",
-            "color": "orange",
-            "country": "Egypt"
-        }
-    }
-}
-    """
-    yaml_text = """
-        name: "Vishvajit"
-        age: 23
-        address: Noida
-        Occupation: Software Developer
-        Skills:
-        - Python
-        - Django
-        - Flask
-        - FastAPI
-        - DRF ( Django Rest Framework )
-    """
-    toml_text = """
-        [user]
-        player_x.color = "blue"
-        player_o.color = "green"
-
-        [constant]
-        board_size = 3
-
-        [server]
-        url = "https://tictactoe.example.com"
-    """
-    ini_text = """
-        [apple]
-        size = small
-        color = red
-        country = USA
-    """
-    sample_dict = {"Vegetable": {"carrot":{"size": "cylindrical", "color": "orange", "country": "America"}}}
-    a = ConfigReader(json_text)
-    b = ConfigReader(ini_text)
-    c = ConfigReader(yaml_text)
-    d = ConfigReader(toml_text)
-    print(a.merge_dictionaries(sample_dict))
-    #print(c.parse_yaml())
-    #print(d.parse_toml())
-    #print(b.parse_ini())
