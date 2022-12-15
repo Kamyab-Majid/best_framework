48f7eeab1ad22b307d04c4a76fc6bd6e5aa34185
MKamyab1991 <majid.kamyab@adastragrp.com>
2022-12-15 19:45:59 +0000

    mod: modified lambda import test file so that pytest can run it.

--- tests/unit/import_tests.py
+++ tests/unit/import_tests.py
@@ -1,4 +1,5 @@
 
+import uuid
 import json
 import subprocess
 import boto3
@@ -10,10 +11,8 @@ def test_lambda_imports():
     current_path = Path.cwd()
     abs_current_path = current_path.resolve()
 
-    upper_path = abs_current_path.parent
-    upper_upper_path = upper_path.parent
-    layer_zip_path = abs_current_path.joinpath('dist', 'lambda_layer.zip')
-    lambda_code_path = abs_current_path.joinpath('tests','test_files', 'import_test_files', 'lambda_function.zip')
+    layer_zip_path = abs_current_path.joinpath/'tests'/'test_files'/'import_test_files'/'lambda_layer.zip'
+    lambda_code_path = abs_current_path.joinpath/'tests'/'test_files'/'import_test_files'/'lambda_function.zip'
     iam = boto3.client('iam')
     role_name = 'lambda-role'
     policy_name = 'my_lambda_policy'
@@ -33,7 +32,7 @@ def test_lambda_imports():
     role_dict = json.loads(get_arn_output.stdout.decode())
     role_arn = role_dict['Role']['Arn']
 
-    my_managed_policy = {
+    lambda_policy = {
         "Version": "2012-10-17",
         "Statement": [
             {
@@ -55,7 +54,7 @@ def test_lambda_imports():
     try:
         policy_response = iam.create_policy(
             PolicyName=policy_name,
-            PolicyDocument=json.dumps(my_managed_policy)
+            PolicyDocument=json.dumps(lambda_policy)
         )
     except:
         print('Policy already exists')
@@ -123,3 +122,159 @@ def test_lambda_imports():
     delete_role_response = iam.delete_role(
         RoleName=role_name,
     )
+
+
+def test_glue_imports():
+    current_path = Path.cwd()
+    abs_current_path = current_path.resolve()
+
+    upper_path = abs_current_path.parent
+    upper_upper_path = upper_path.parent
+    layer_zip_path = abs_current_path.joinpath('dist', 'glue_layer.zip')
+    glue_code_path = abs_current_path.joinpath('tests', 'test_files', 'import_test_files', 'glue_job.zip')
+    iam = boto3.client('iam')
+    role_name = 'glue-role'
+    policy_name = 'glue_policy'
+    function_name = 'test_glue_job'
+    region = 'ca-central-1'
+    zip_file_name = 'my_layer.zip'
+    layer_name = 'my_layer'
+    # Get the account_id
+    sts = boto3.client('sts')
+    identity = sts.get_caller_identity()
+
+    pass
+
+import uuid
+role_name = 'glue-role'
+policy_name = 'glue_policy'
+function_name = 'test_glue_job'
+region = 'ca-central-1'
+zip_file_name = 'my_layer.zip'
+layer_name = 'my_layer'
+bucket_name = 'glue-test-bucket'+uuid.uuid4().hex
+region='ca-central-1'
+# Create a boto3 client for the Glue service
+iam=boto3.client('iam')
+
+# Define the role's name and trust policy
+role_name='MyGlueRole'
+trust_policy={
+    'Version': '2012-10-17',
+    'Statement': [
+        {
+            'Effect': 'Allow',
+            'Principal': {
+                'Service': 'glue.amazonaws.com'
+            },
+            'Action': 'sts:AssumeRole'
+        }
+    ]
+}
+
+# Create the Glue role
+role_response = iam.create_role(
+    RoleName = role_name,
+    AssumeRolePolicyDocument = json.dumps(trust_policy)
+)
+s3=boto3.client('s3')
+
+# Create a new bucket
+bucket_response=s3.create_bucket(
+    Bucket = bucket_name,
+    CreateBucketConfiguration = {
+        'LocationConstraint': region
+    }
+)
+# Print the ARN of the newly created role
+print(role_response['Role']['Arn'])
+# Define the policy document
+policy_document={
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "GlueJobPermissions",
+            "Effect": "Allow",
+            "Action": [
+                "glue:GetJob",
+                "glue:BatchStopJobRun",
+                "glue:StartJobRun"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowBucketReadAccess",
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket",
+                "s3:GetObject"
+            ],
+            "Resource": [
+                f"arn:aws:s3:::{bucket_name}",
+                f"arn:aws:s3:::{bucket_name}/*"
+            ]
+        }
+    ]
+}
+sts = boto3.client('sts')
+identity = sts.get_caller_identity()
+account_id = (identity['Account'])
+try:
+    policy_response = iam.create_policy(
+        PolicyName = policy_name,
+        PolicyDocument = json.dumps(policy_document)
+    )
+except:
+    print('Policy already exists')
+policy_arn = f'arn:aws:iam::{account_id}:policy/{policy_name}'
+# Attach the policy to the role
+
+iam.attach_role_policy(
+    RoleName=role_name,
+    PolicyArn=policy_arn
+)
+
+
+# Create an S3 client
+
+current_path = Path.cwd()
+abs_current_path = current_path.resolve()
+layer_zip_path = abs_current_path/'tests'/'test_files'/'import_test_files'/'glue_zip.zip'
+glue_code_path = abs_current_path/'tests'/'test_files'/'import_test_files'/'glue_import_test.py'
+s3.upload_file(str(layer_zip_path), bucket_name, 'glue_zip.zip')
+s3.upload_file(str(glue_code_path), bucket_name, 'glue_import_test.py')
+glue_config = {
+    "Command": {
+        "Name": "glueetl",
+        "PythonVersion": "3",
+        "ScriptLocation": f"s3://{bucket_name}/glue_import_test.py"
+    },
+    "DefaultArguments": {
+        "--TempDir": f"s3://aws-glue-assets-{account_id}-{region}/temporary/",
+        "--enable-continuous-cloudwatch-log": "true",
+        "--enable-glue-datacatalog": "true",
+        "--enable-job-insights": "true",
+        "--enable-metrics": "true",
+        "--enable-spark-ui": "false",
+        "--extra-py-files": f"s3://{bucket_name}/glue_zip.zip",
+        "--job-bookmark-option": "job-bookmark-enable",
+        "--job-language": "python",
+        "--spark-event-logs-path": f"s3://aws-glue-assets-{account_id}-{region}/sparkHistoryLogs/"
+    },
+    "Description": "",
+    "ExecutionProperty": {
+        "MaxConcurrentRuns": 1
+    },
+    "GlueVersion": "3.0",
+    "MaxRetries": 0,
+    "Name": "glue_import_test",
+    "NumberOfWorkers": 2,
+    "Role": f"arn:aws:iam::{account_id}:role/service-role/{role_name}",
+    "Timeout": 5,
+    "WorkerType": "G.1X"
+}
+glue_client = boto3.client('glue')
+glue_client.create_job(**glue_config)
+glue_client.start_job_run(JobName=glue_config['Name'])
+glue_client.get_job_runs(JobName=glue_config['Name'])
+glue_client.delete_job(JobName=glue_config['Name'])
